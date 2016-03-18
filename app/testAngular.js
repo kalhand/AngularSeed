ba007f76ca51e000e37af33acdfe3279e012a9d9
kalhand <kalhan.d@gmail.com>
2016-03-18 16:48:02 +0530

    Angular Seed

---
+++ app/testAngular.js
@@ -0,0 +1,48 @@
+describe('When access Controller', function () {
+    beforeEach(module('app'));
+
+    var $controller,$filter,commentService;
+
+    beforeEach(inject(function (_$controller_,_$filter_,_commentService_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+        $filter = _$filter_;
+        commentService = _commentService_;
+    }));
+
+    describe('Test comment service', function () {
+      it('should have Auth service be defined', function () {
+          expect(commentService).toBeDefined();
+      });
+      it('should save a user', function() {
+        var comment = {'text' : 'abcd','id' : '','time':'','active' : ''};
+
+        commentService.save(comment);
+        var comments = commentService.list();
+        expect(comments[0].text).toBe(comment.text);
+        expect(comments[0].active).toBe(false);
+      });
+    });
+
+    describe('Test comment Controller', function () {
+        it('Get Initail List', function () {
+            var controller = $controller('commentController');
+            expect(controller.comments).toEqual([]);
+        });
+        it('Save Data', function () {
+            var controller = $controller('commentController');
+            controller.newcomment = {'text' : 'abcd','id' : '','time':'','active' : ''}
+            controller.savecomment();
+            expect(controller.comments.length).toEqual(1);
+        });
+        
+    });
+
+    describe('Test comment Service', function () {
+        it('Get Initail List', function () {
+            var controller = $controller('commentController',{});
+            expect(controller.comments).toEqual([]);
+        });
+    });
+
+});
